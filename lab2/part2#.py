e3985f9b6cebda8544325bbc4a92dd25d0a6cbbd
kulbitmc <71315477+kulbitmc@users.noreply.github.com>
2020-09-23 00:00:44 +0300

    Create part2#.py

---
+++ lab2/part2#.py
@@ -0,0 +1,16 @@
+from random import randint
+import turtle
+
+number_of_turtles = 20
+steps_of_time_number = 100
+
+pool = [turtle.Turtle(shape='circle') for i in range(number_of_turtles)]
+for unit in pool:
+    unit.penup()
+    unit.speed(50)
+    unit.goto(randint(-200, 200), randint(-200, 200))
+
+for i in range(steps_of_time_number):
+    for unit in pool:
+        unit.setheading(randint(0,360))
+        unit.forward(randint(-60,60))
